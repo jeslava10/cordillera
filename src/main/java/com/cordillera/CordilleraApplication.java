3da68ea71ce0a27c3db5df824571bd32a6cebfc9
Juan.Eslava <juaneslava56@gmail.com>
2022-07-02 17:40:30 -0500

    Proyecto base

---
+++ src/main/java/com/cordillera/CordilleraApplication.java
@@ -0,0 +1,13 @@
+package com.cordillera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CordilleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CordilleraApplication.class, args);
+	}
+
+}
