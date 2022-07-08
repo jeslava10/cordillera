914155146fe8d508fb0ecde71022d7c47472fa7f
Oscar Andres Ramos Lopez <oscar.ramos@siesa.com>
2022-07-07 22:48:15 -0500

    mejora para la conexion db

--- src/main/java/com/cordillera/domain/models/User.java
+++ src/main/java/com/cordillera/domain/models/User.java
@@ -2,12 +2,10 @@ package com.cordillera.domain.models;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name = "User")
+@Table(name = "usercodillera")
 @Builder
 @AllArgsConstructor()
 @NoArgsConstructor
@@ -16,6 +14,7 @@ import javax.persistence.Table;
 public class User {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
