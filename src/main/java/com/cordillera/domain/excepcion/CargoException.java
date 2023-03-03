a4a19b4a3faab53b10af5b546b3676ff0ad7b100
Cesar Tello <cesar.tello@correounivalle.edu.co>
2023-03-03 18:54:37 -0500

    Correciones Métodos de validación y excepciones personalizadas en Cargo 4

--- src/main/java/com/cordillera/domain/excepcion/CargoException.java
+++ src/main/java/com/cordillera/domain/excepcion/CargoException.java
@@ -2,27 +2,16 @@ package com.cordillera.domain.excepcion;
 
 public class CargoException extends RuntimeException{
     private static final long serialVersionUID = 1564668989254L;
-    private String message;
 
     public CargoException() {
     }
 
     public CargoException(String message) {
         super(message);
-        this.message = message;
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     @Override
     public String toString() {
-        return "CargoNofoundException [message=" + message + "]";
+        return "CargoNofoundException [message=" + this.getMessage() + "]";
     }
 }
