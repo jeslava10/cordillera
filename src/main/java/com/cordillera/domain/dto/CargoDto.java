02abe26c7a2c76f7d17645051b7e558444cd9d49
Cesar Tello <cesar.tello@correounivalle.edu.co>
2023-03-03 18:16:30 -0500

    Correciones Métodos de validación y excepciones personalizadas en Cargo 3

--- src/main/java/com/cordillera/domain/dto/CargoDto.java
+++ src/main/java/com/cordillera/domain/dto/CargoDto.java
@@ -7,8 +7,6 @@ import javax.validation.constraints.NotNull;
 @Data
 public class CargoDto {
 
-    //TODO AJUSTAR CON CAMBIOS REALIZADOS EN ENTIDAD
-    //TODO  ASIGANAR MENSAJE EN EL @NOTNULL
     @NotNull
     private Long idCargo;
     @NotNull
